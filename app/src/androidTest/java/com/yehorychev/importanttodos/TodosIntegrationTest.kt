b9d8d7a0da33ef96094a663016bd64776dc6e577
Yehor Yehorychev <egor57899@gmail.com>
2024-12-25 10:59:59 -0800

    Assertion has been added

--- app/src/androidTest/java/com/yehorychev/importanttodos/TodosIntegrationTest.kt
+++ app/src/androidTest/java/com/yehorychev/importanttodos/TodosIntegrationTest.kt
@@ -1,12 +1,15 @@
 package com.yehorychev.importanttodos
 
-import androidx.compose.ui.test.hasText
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.allOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -23,5 +26,8 @@ class TodosIntegrationTest {
         // Add new todo item
         onView(withId(R.id.todo_title)).perform(typeText(newTodoText))
         onView(withId(R.id.todo_save_button)).perform(click())
+
+        // Assertion
+        onView(allOf(withId(R.id.todo_name), withText(newTodoText))).check(matches(isDisplayed()))
     }
 }
